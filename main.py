edfc4664a887a3e0554021c7aed8476fbd4e334f
Rose Perrone <rose0x4@gmail.com>
2014-10-02 11:44:57 -0700

    Writes the video frames to files

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 
-from os import system
+from os import system, listdir
+from os.path import dirname, abspath
 
 from video_utils import get_video, get_prepared_images
 
 import pdb
+
+ROOT = dirname(abspath(__file__))
 
 def where_is_noun_in_video(url, noun):
   '''
@@ -11,16 +14,12 @@ def where_is_noun_in_video(url, noun):
     a list of tuples of video segments in which that noun appears,
     in seconds. e.g. [(14.4, 20.2), (34.2, 89.2)]
   '''
-  video_filename = get_video(url)
-  images = get_prepared_images(video_filename)
-  noun_detected_times = []
-  for image, time in images:
-    detected_nouns = detect(image)
-    if noun in detected_nouns:
-      noun_detected_times.append(time)
+  # TODO change 10,000 to 1,000 after the full pipeline works
+  image_dir = get_prepared_images(url, 10000)
+  noun_detected_times = detect(image_dir)
   return approximate_video_segments(noun_detected_times)
 
-def detect(image):
+def detect(image_dir):
   '''
   Uses the 19-layer neural net from:
     Very Deep Convolutional Networks for Large-Scale Image Recognition
@@ -32,9 +31,14 @@ def detect(image):
   Returns:
 
   '''
-  root = dirname(abspath(__file__))
-  cmd = root + '/caffe/python/detect.py '
-  cmd += ''
+  image_filenames_txt = '/tmp/image_filenames.txt'
+  with open(image_filenames_txt, 'w') as f:
+    # skip the mac .DS_Store file
+    image_filenames = [x for x in listdir(image_dir) if not x == '.DS_Store']
+    f.write('\n'.join(image_filenames))
+  cmd = ROOT + '/caffe/python/detect.py'
+  pdb.set_trace()
+  cmd += ' --input_file=' + image_filenames_txt
 
 def approximate_video_segments(times):
   '''
--- video_utils.py
+++ video_utils.py
@@ -1,7 +1,7 @@
 '''
 Utility methods for downloading and preparing video frames.
 '''
-from os.path import dirname, abspath, exists, splitext, basename
+from os.path import dirname, abspath, exists, splitext, basename, join
 from os import system
 from PIL import Image
 import numpy as np
@@ -15,49 +15,65 @@ import pdb
 
 ROOT = dirname(abspath(__file__))
 
+def get_prepared_images(url, ms_between_frames, use_cache=True):
+  '''
+  Returns:
+    The  directory that contains the image frames.
+    The image filenames take the format:
+      <milliseconds_into_the_video>.jpg
+    The directory name is stored in data/images and takes the format:
+      <sanitized video url>_<milliseconds_between_frames>
+
+  '''
+  image_dir = image_frames_dir(url, ms_between_frames)
+  if exists(image_dir):
+    if use_cache:
+      return image_dir
+    else:
+      system('rm -rf ' + image_dir)
+  system('mkdir -p ' + image_dir)
+  video_filename = get_video(url)
+  cap = cv2.VideoCapture(video_filename)
+  frame_count = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+  frames_per_s = int(cap.get(cv2.cv.CV_CAP_PROP_FPS))
+
+  step = (ms_between_frames / 1000) * frames_per_s
+  image_count = 0
+  for i in range(frame_count)[::step]:
+    cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, i)
+    retval, image = cap.read()
+    filename = join(image_dir, str(image_count * ms_between_frames) + '.jpg')
+    Image.fromarray(prepare_image(Image.fromarray(image))).save(filename,
+                                                            format='JPEG')
+    image_count += 1
+    print image_count, '/', frame_count/step
+  return image_dir
+
+def image_frames_dir(url, ms_between_frames):
+  return join(ROOT, 'data/images', get_video_id(url) + '_' +
+              str(ms_between_frames))
+
 def get_video(url):
   '''
   Returns the filename of the downloaded mp4
   '''
   video = pafy.new(url)
-  stream = _get_stream(video)
-  filename = _downloaded_filename(url, video.title, stream.extension)
+  stream = get_stream(video)
+  filename = downloaded_filename(url, video.title, stream.extension)
   # First check if we cached it
-  mp4_filename = _converted_mp4_filename(filename)
+  mp4_filename = converted_mp4_filename(filename)
   if exists(mp4_filename):
     return mp4_filename
   stream.download(filename)
-  return _convert_video_to_mp4(filename)
+  return convert_video_to_mp4(filename)
 
-def get_prepared_images(video_filename):
-  '''
-  Returns:
-    A list of tuples of the format:
-      (image, time)
-    where `time` is the time at which the image appears in the video,
-    in seconds.
-  '''
-  cap = cv2.VideoCapture(video_filename)
-  frame_count = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
-  frames_per_s = int(cap.get(cv2.cv.CV_CAP_PROP_FPS))
-
-  images = []
-  # TODO take out the 10x after the full pipeline works
-  for i in range(frame_count)[::10*frames_per_s]:
-    cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, i)
-    retval, image = cap.read()
-    images.append(_prepare_image(Image.fromarray(image)))
-    print len(images), '/', frame_count/(10*frames_per_s)
-  return images
-
-def _prepare_image(image):
+def prepare_image(image):
   'Resizes the images to 256x256 and demeans the images.'
-  image = _resize_and_paste(image, (256, 256))
+  image = resize_and_paste(image, (256, 256))
   image = np.asarray(image)
-  retval = image - [103.939, 116.779, 123.68]
-  return retval
+  return (image - np.array([103.939, 116.779, 123.68])).astype(np.uint8)
 
-def _resize_and_paste(image, new_shape):
+def resize_and_paste(image, new_shape):
   '''
   The original image proportions are left unchanged; any left over area
   is filled with black.
@@ -81,11 +97,11 @@ def _resize_and_paste(image, new_shape):
     result.paste(image, ((new_width - cw) / 2, 0))
   return result
 
-def _show_image(image):
+def show_image(image):
   pylab.imshow(image)
   pylab.show()
 
-def _get_stream(video):
+def get_stream(video):
   'Gets the lowest resolution stream that has the smallest dimension >= 256'
   # my ffmpeg build doesn't convert webm's, so I'm just using m4v for now
   dimensions = [stream.dimensions for stream in video.videostreams \
@@ -99,18 +115,22 @@ def _get_stream(video):
     if stream.resolution == str(res[0]) + 'x' + str(res[1]):
       return stream
 
-def _convert_video_to_mp4(filename):
+def convert_video_to_mp4(filename):
   'OpenCV seems to only like mp4'
-  target = _converted_mp4_filename(filename)
+  target = converted_mp4_filename(filename)
   print target
   system('ffmpeg -i ' + filename + ' -vcodec copy ' + target)
   return target
 
-def _converted_mp4_filename(filename):
+def converted_mp4_filename(filename):
   return ROOT + '/data/videos/' + splitext(basename(filename))[0] + '.mp4'
 
-def _downloaded_filename(url, title, extension):
-  return dirname(abspath(__file__)) + '/data/videos/' + \
-         re.match('.*v=(.*$)', url).groups()[0] + \
-         ''.join(x for x in title if x.isalnum()) + '.' + extension
+def downloaded_filename(url, title, extension):
+  return dirname(abspath(__file__)) + '/data/videos/' + get_video_id(url) + \
+         sanitized_video_title(title) + '.' + extension
 
+def sanitized_video_title(title):
+  return ''.join(x for x in title if x.isalnum())
+
+def get_video_id(url):
+  return re.match('.*v=(.*$)', url).groups()[0]
